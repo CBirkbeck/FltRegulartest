0525f3a1732a178e746ca1d49c7a46151bc6ba13
Chris Birkbeck <c.birkbeck@ucl.ac.uk>
2023-10-04 12:49:10 +0100

    locally working maybe

--- lakefile.lean
+++ lakefile.lean
@@ -7,7 +7,7 @@ package «FltRegular» where
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
-meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
+
 require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"    
 
 @[default_target]
